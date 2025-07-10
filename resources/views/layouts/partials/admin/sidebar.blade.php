a15d830eaf6278553cf76fb02f93e15ca1dbaaa0
Yuritzi Calixto <ramirezcalixtodiana@gmail.com>
2025-07-10 13:41:44 -0600

    Proteger enlaces del sidebar

--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -4,33 +4,38 @@
         'icon' => 'fa-solid fa-gauge',
         'name' => 'Dashboard',
         'route' => route ('admin.dashboard'),
-        'active' => request()->routeIs('admin.dashboard')
+        'active' => request()->routeIs('admin.dashboard'),
+        'can' => ['access_dashboard']
     ],
     [
         //Usuarios
         'icon' => 'fa-solid fa-box-open',
         'name' => 'Usuarios',
         'route' => route ('admin.usuarios-unity'),
-        'active' => request()->routeIs('admin.usuarios-unity')
+        'active' => request()->routeIs('admin.usuarios-unity'),
+        'can' => ['manage_users']
     ],
     [
         //Progreso de Unity Nivel, Tiempo y id del Usuario
         'icon' => 'fa-solid fa-fire-flame-curved',
         'name' => 'Progreso Unity',
         'route' => route ('admin.progreso-unity'),
-        'active' => request()->routeIs('admin.progreso-unity')
+        'active' => request()->routeIs('admin.progreso-unity'),
+        'can' => ['manage_progreso']
     ],
     [
         'icon' => 'fa-solid fa-fire-flame-curved',
         'name' => 'Usuarios Totales',
         'route' => route ('admin.users.index'),
-        'active' => request()->routeIs('admin.users.*')
+        'active' => request()->routeIs('admin.users.*'),
+        'can' => ['manage_users']
     ],
     [
         'icon' => 'fa-solid fa-user-tag',
         'name' => 'Roles',
         'route' => route ('admin.roles.index'),
-        'active' => request()->routeIs('admin.roles.*')
+        'active' => request()->routeIs('admin.roles.*'),
+        'can' => ['manage_roles']
     ],
 
 ];
@@ -46,22 +51,25 @@
     aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
-
+{{--ITERANDO ENLACES--}}
             @foreach ($links as $link)
-                <li>
-                    {{--  --}}
-                    <a href="{{ $link['route'] }}"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : ''}}">
+                @canany($link['can'] ?? [null])
 
-                        <span class="inline-flex w-6 h-6 justify-center items-center">
-                            <i class=" {{$link['icon']}} text-gray-500"></i>
-                        </span>
+                    <li>
+                        {{--  --}}
+                        <a href="{{ $link['route'] }}"
+                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : ''}}">
 
-                        <span class="ms-2">
-                            {{$link['name']}}
-                        </span>
-                    </a>
-                </li>
+                            <span class="inline-flex w-6 h-6 justify-center items-center">
+                                <i class=" {{$link['icon']}} text-gray-500"></i>
+                            </span>
+
+                            <span class="ms-2">
+                                {{$link['name']}}
+                            </span>
+                        </a>
+                    </li>
+                @endcanany
             @endforeach
         </ul>
     </div>
